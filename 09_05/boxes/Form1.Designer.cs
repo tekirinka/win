53425293147fedd002657b2998263ce0bbc685d3
asd <asd@c.vb>
2019-09-08 12:04:17 +0300

    init

---
+++ 09_05/boxes/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace boxes
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn = new System.Windows.Forms.Button();
+            this.pb = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn
+            // 
+            this.btn.AutoSize = true;
+            this.btn.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btn.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btn.Location = new System.Drawing.Point(0, 512);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(785, 33);
+            this.btn.TabIndex = 1;
+            this.btn.Text = "Draw";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.draw);
+            // 
+            // pb
+            // 
+            this.pb.BackColor = System.Drawing.Color.White;
+            this.pb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pb.Location = new System.Drawing.Point(0, 0);
+            this.pb.Name = "pb";
+            this.pb.Size = new System.Drawing.Size(785, 512);
+            this.pb.TabIndex = 2;
+            this.pb.TabStop = false;
+            this.pb.LoadCompleted += new System.ComponentModel.AsyncCompletedEventHandler(this.draw);
+            this.pb.Click += new System.EventHandler(this.draw);
+            this.pb.Resize += new System.EventHandler(this.draw);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(785, 545);
+            this.Controls.Add(this.pb);
+            this.Controls.Add(this.btn);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 12F);
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.MaximizeBox = false;
+            this.Name = "Form";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Boxes";
+            this.Load += new System.EventHandler(this.draw);
+            this.Shown += new System.EventHandler(this.draw);
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.PictureBox pb;
+    }
+}
+
---
+++ 09_05/boxes/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace boxes
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void draw()
+        {
+            Graphics g = pb.CreateGraphics();
+
+            float size = Math.Min(pb.Width, pb.Height) / 5;
+
+            SizeF box = new SizeF(size, size);
+
+            PointF center = new PointF((pb.Width - size) / 2, (pb.Height - size) / 2);
+
+            float top = center.Y - size;
+            float bottom = center.Y + size;
+            float left = center.X - size;
+            float right = center.X + size;
+
+            PointF tl = new PointF(left, top);
+            PointF tr = new PointF(right, top);
+            PointF bl = new PointF(left, bottom);
+            PointF br = new PointF(right, bottom);
+
+            RectangleF c = new RectangleF(center, box);
+            RectangleF tlr = new RectangleF(tl, box);
+            RectangleF trr = new RectangleF(tr, box);
+            RectangleF blr = new RectangleF(bl, box);
+            RectangleF brr = new RectangleF(br, box);
+
+
+            RectangleF[] rects = { c, tlr, trr, blr, brr };
+
+            g.Clear(Color.White);
+
+            g.FillRectangles(Brushes.Black, rects);
+        }
+
+        private void draw(object sender, EventArgs e) => draw();
+
+        private void draw(object sender, AsyncCompletedEventArgs e) => draw();
+    }
+}
